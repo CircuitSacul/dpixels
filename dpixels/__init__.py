3f7b543d05884fac0339ae7ba5190576d7b9c8d2
Circuit <55721663+CircuitSacul@users.noreply.github.com>
2021-05-28 22:58:03 -0400

    Update __init__.py

--- dpixels/__init__.py
+++ dpixels/__init__.py
@@ -5,7 +5,7 @@ from .color import Color
 from .exceptions import *  # NOQA
 from .ratelimits import RateLimitedEndpoint, Ratelimits
 
-__version__ = "0.0.6"
+__version__ = "0.0.7"
 
 __all__ = [
     "Canvas",
